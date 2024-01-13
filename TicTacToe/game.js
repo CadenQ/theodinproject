2c727a918f05d1a72878c4d00595a89153c8ffd2
Caden Quiring <cadenq1@gmail.com>
2024-01-13 11:11:48 -0500

    A bit messy, I just wanted it to be over

--- TicTacToe/game.js
+++ TicTacToe/game.js
@@ -1,33 +1,76 @@
+/**
+ * This module contains all logical operations to run the game itself
+ * @module Game
+ * @param gameboard - A 2D array containing the content of each square
+ * @param checkSquare - Checks if a square is available to play in
+ * @param markSquare - Marks a square with the given player if the square is available
+ * @param checkGame - Checks if there is a winner
+ */
 class Game {
+    /**
+     * Constructor of the game, instantiates the gameboard
+     */
     constructor() {
+        /** Gameboard that contains all values of the squares */
         this.gameboard = [[null, null, null], [null, null, null], [null, null, null]];
     }
-    
-    markSquare(player, square) {
-        console.log(square);
-        console.log(this.gameboard);
-        this.gameboard[parseInt(square[0])][parseInt(square[1])] = parseInt(player);
+
+    /**
+     * Checks if a square is available to play in
+     * @param {Array} square 
+     * @returns boolean of if the square is available 
+     */
+    checkSquare(square) {
+        if (this.gameboard[square[0]][square[1]] !== null)
+            return false;
+        return true;
     }
 
+    /**
+     * Marks a square with the given player if the square is available
+     * @param {string} player 
+     * @param {Array} square 
+     * @returns true if a square was marked, and false if a square wasn't marked 
+     */
+    markSquare(player, square) {
+        if (this.checkSquare(square)) {
+            this.gameboard[square[0]][square[1]] = player;
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Checks if there is a winner
+     * @returns X if X won, O if O won, null if it's a Cat's Game, and 0 if no one has won yet
+     */
     checkGame() {
         // Check rows
-        this.gameboard.forEach(element => {
-            if (element.every(n => n == 1))
-                return 1;
-            else if (element.every(n => n == 2))
-                return 2;
-        });
+        for (var row of this.gameboard) {
+            if (row.every(n => n == "X"))
+                return "X";
+            else if (row.every(n => n == "O"))
+                return "O";
+        }
 
         // Check columns
         for (let i = 0; i < this.gameboard.length; i++) 
-            if (this.gameboard[i][0] == this.gameboard[i][1] && this.gameboard[i][1] == this.gameboard[i][2])
+            if (this.gameboard[0][i] !== null && this.gameboard[0][i] == this.gameboard[1][i] && this.gameboard[1][i] == this.gameboard[2][i]) 
                 return this.gameboard[i][0];
+            
 
         // Check diagonals
-        if ((this.gameboard[0][0] == this.gameboard[1][1] && this.gameboard[1][1] == this.gameboard[2][2]) ||
-            (this.gameboard[0][2] == this.gameboard[1][1] && this.gameboard[1][1] == this.gameboard[2][0]))
+        if ((this.gameboard[0][0] !== null && this.gameboard[0][0] == this.gameboard[1][1] && this.gameboard[1][1] == this.gameboard[2][2]) ||
+            (this.gameboard[0][2] !== null && this.gameboard[0][2] == this.gameboard[1][1] && this.gameboard[1][1] == this.gameboard[2][0]))
             return this.gameboard[1][1];
+
+        let catsGame = 0;
+        // Check cat's game
+        for (var row of this.gameboard) 
+            if (row.every(n => n !== null)) 
+                catsGame++;
         
-        return 0;    
+        return catsGame == 3 ? null : 0;    
     }
 }
--- TicTacToe/main.js
+++ TicTacToe/main.js
@@ -1,26 +1,68 @@
+/**
+ * Main class that handles the game
+ * @module GameController
+ * @param {Game} currentGame - Instance of the Game class
+ * @param {string} currentPlayer - The current player of the game (X or O)
+ * @param optionClicked - Handles the player clicking an option
+ * @param reset - Resets the game
+ */
 class GameController {
+    /**
+     * Constructs with two variables, currentGame and currentPlayer
+     */
     constructor() {
         this.currentGame = new Game();
         this.currentPlayer = "X";
     }
 
+    /** 
+     * Handles the square being selected by the player 
+     * @param option - What square the player chose
+     * @param player - Which player chose the square
+     */
     optionClicked(option, player) {
-        option.innerHTML = player;
-        console.log(option);
-        this.currentGame.markSquare(player, option.id.split("-"));
-        console.log(this.currentGame.checkGame());
+        if (this.currentGame.checkGame() == 0) {
+            if (this.currentGame.markSquare(player, option.id.split("-"))) {
+                option.innerHTML = player;
+                console.log(this.currentGame.checkGame());
+                this.currentPlayer = this.currentPlayer == "X" ? "O" : "X";
+            }
+       }
     }
 
+    /**
+     * Resets the game
+     */
     reset() {
         this.currentGame = new Game();
+        for (var box of document.getElementById("game").children)
+            box.innerHTML = "";
+        document.getElementById("winner").innerHTML = "";
     }
     
 }
 
-let game = new GameController();
+// Main game variable
+const game = new GameController();
 
+// Event handler to reset the game
+document.getElementById("reset").addEventListener("click", () => {
+    game.reset();
+    console.clear();
+});
+
+// Adds event handlers to every square to handle player interaction and winner
 for (var box of document.getElementById("game").children) {
-    box.addEventListener("click", () => {
-        game.optionClicked(box, game.currentPlayer);
+    box.addEventListener("click", function() {
+        // Handles if a player clicked an available square
+        game.optionClicked(this, game.currentPlayer);
+
+        // If someone has won the game, display the winner
+        let gameState = game.currentGame.checkGame();
+        if (gameState != 0)
+            if (gameState === null)
+                document.getElementById("winner").innerHTML = "Cat's Game!";
+            else
+                document.getElementById("winner").innerHTML = game.currentPlayer == "X" ? "O" : "X";
     });
 }
