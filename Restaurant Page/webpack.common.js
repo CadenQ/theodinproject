2f3bec7d78724a1a6dbf0bba17c156f47b9bc971
Caden Quiring <caden.quiring@cntrline.com>
2024-01-15 11:25:34 -0500

    This project seems like fun, so I want to come back to it. I'd really rather spend as much time as possible trying to learn react though, this will take up too much time right now.

--- Restaurant Page/webpack.common.js
+++ Restaurant Page/webpack.common.js
@@ -1,13 +1,13 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
     entry: './src/index.js',
     output: {
-        filename: 'main.js',
+        filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
         clean: true,
     },
-    devtool: 'inline-source-map',
     plugins: [
         new HtmlWebpackPlugin({
             template: './src/index.html',
@@ -15,5 +15,16 @@ module.exports = {
             inject: 'body',
         }),
     ],
-    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
 };
